5798b29dbee07320aaf18f8ef39e275e6377ba31
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2020-11-07 17:47:50 +0000

    asda

--- js/mainGame.js
+++ js/mainGame.js
@@ -392,9 +392,9 @@ function init() {
 				wallGeometry2 = new THREE.PlaneGeometry(100, 100);
 				for (var i = 0, l = wallGeometry2.vertices.length; i < l; i++) {
 					var vertex = wallGeometry2.vertices[i];
-					vertex.x += 150 - 5;
+					vertex.x += 30 - 5;
 					vertex.y += 3 + 40;
-					vertex.z += 30 - 80;
+					vertex.z += 200 - 80;
 				}
 
 				// Creates floor geometry
